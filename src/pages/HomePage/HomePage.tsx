9f2f99ac7a7115279bacc1801faa863f68c3b691
LAPCUAMINH\minhm <pnminh1721@gmail.com>
2024-07-23 13:10:27 +0700

    update ui v0.2

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,15 @@
 import PATH from '@/constants/path'
-import { Space } from 'antd'
-import { Link } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 export default function HomePage() {
-  return (
-    <p>
-      <h1>Home</h1>
-      <Space>
-        <Link to={PATH.TEXAS_COWBOY}>Texas Cowboy</Link>
-        <Link to={PATH.BUSINESS_AND_STRATEGY}>Business & Strategy</Link>
-      </Space>
-    </p>
-  )
+  return <Navigate to={PATH.TEXAS_COWBOY} />
+  // return (
+  //   <p>
+  //     <h1>Home</h1>
+  //     <Space>
+  //       <Link to={PATH.TEXAS_COWBOY}>Texas Cowboy</Link>
+  //       <Link to={PATH.BUSINESS_AND_STRATEGY}>Business & Strategy</Link>
+  //     </Space>
+  //   </p>
+  // )
 }
